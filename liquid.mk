d76d069cb2b0a056bc03f5c2a407ba57f44effd1
itsmikeramsay <ronaldramsayii@gmail.com>
2014-09-04 11:37:26 -0400

    hltevzw: common_phone
    Change-Id: Icd2c0f5df79215327f42a645f67378ea5c1de28b

--- liquid.mk
+++ liquid.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/samsung/hltevzw/full_hltevzw.mk)
 $(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 PRODUCT_DEVICE := hltevzw
 PRODUCT_NAME := liquid_hltevzw
