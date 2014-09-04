701c0d6537c5155ffd150d544becf4770b952021
itsmikeramsay <ronaldramsayii@gmail.com>
2014-09-04 11:00:23 -0400

    hltevzw: Import remaining files needed for hltevzw tree
    Change-Id: I313d3d91d0491e3024a14819b2d5c8c216d890fd

---
+++ liquid.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/samsung/hltevzw/full_hltevzw.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := hltevzw
+PRODUCT_NAME := liquid_hltevzw
